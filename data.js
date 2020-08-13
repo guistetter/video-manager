cefbe7085919d405c976e657ab6a2c1786d8f685
guistetter <guilherme_stetter@hotmail.com>
2020-08-13 13:34:00 -0300

    alterando id dos vidoes, incluindo novos videos para testes..

--- data.js
+++ data.js
@@ -7,34 +7,34 @@ module.exports = [{
   featured: true
   },
   {
-  id: "jbAh5R8CH_o",
+  id: "Wiha271NDI4",
   imgName:"imagem2.jpg",
   title: "Formulário animado com JS puro e Animation | Jquery, Javascript, REACT, REACT NATIVE",
   duration: "400 min",
   price: "Free"
   },
   {
-  id: "jbAh5R8CH_o",
+  id: "8VWFgaxSng8",
   imgName:"imagem3.jpg",
   title: "Criando aplicacoes WEB com REACT JS",
   duration: "57 min",
   price: "Free"
   },
   {
-  id: "jbAh5R8CH_o",
+  id: "21lgl_2X3zM",
   imgName:"imagem4.jpg",
   title: "Curso Full Stack Javascript, node, frontend, mongoose, gulp, webpack, react",
   duration: "800 min",
   price: "Free"
   },
   {
-  id: "jbAh5R8CH_o",
+  id: "gZHZjn7qekQ",
   imgName:"imagem1.jpg",
   title: "Formulário animado com JS puro e Animation | kel kel",
   duration: "57 min",
   price: "Free"
   },
-  { id: "jbAh5R8CH_o",
+  { id: "8JGa44vdQ8Y",
   imgName:"imagem2.jpg",
   title: "Formulário animado com JS puro e Animation | kel kel",
   duration: "57 min",
