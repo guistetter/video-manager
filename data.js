437368dfecbc8eec3046538077a0d28e73c33606
guistetter <guilherme_stetter@hotmail.com>
2020-08-13 09:33:21 -0300

    criando objeto de dados para simular banco de dados com as informacoes do video

---
+++ data.js
@@ -0,0 +1,42 @@
+module.exports = [{
+  id: "jbAh5R8CH_o",
+  imgName:"imagem1.jpg",
+  title: "Formulário animado com JS puro e Animation | kel kel",
+  duration: "57 min",
+  price: "Free"
+  },
+  {
+  id: "jbAh5R8CH_o",
+  imgName:"imagem1.jpg",
+  title: "Formulário animado com JS puro e Animation | kel kel",
+  duration: "57 min",
+  price: "Free"
+  },
+  {
+  id: "jbAh5R8CH_o",
+  imgName:"imagem1.jpg",
+  title: "Formulário animado com JS puro e Animation | kel kel",
+  duration: "57 min",
+  price: "Free"
+  },
+  {
+  id: "jbAh5R8CH_o",
+  imgName:"imagem1.jpg",
+  title: "Formulário animado com JS puro e Animation | kel kel",
+  duration: "57 min",
+  price: "Free"
+  },
+  {
+  id: "jbAh5R8CH_o",
+  imgName:"imagem1.jpg",
+  title: "Formulário animado com JS puro e Animation | kel kel",
+  duration: "57 min",
+  price: "Free"
+  },
+  { id: "jbAh5R8CH_o",
+  imgName:"imagem1.jpg",
+  title: "Formulário animado com JS puro e Animation | kel kel",
+  duration: "57 min",
+  price: "Free"
+  }
+]
