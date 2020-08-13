73137d302672b524ae1bc16a7341955da9fec87a
guistetter <guilherme_stetter@hotmail.com>
2020-08-13 10:09:54 -0300

    alterando dados para testes

--- data.js
+++ data.js
@@ -1,29 +1,29 @@
 module.exports = [{
   id: "jbAh5R8CH_o",
   imgName:"imagem1.jpg",
-  title: "Formulário animado com JS puro e Animation | kel kel",
+  title: "Aprendendo SQL SERVER do Basico ao Avancado",
   duration: "57 min",
   price: "Free"
   },
   {
   id: "jbAh5R8CH_o",
-  imgName:"imagem1.jpg",
-  title: "Formulário animado com JS puro e Animation | kel kel",
+  imgName:"imagem2.jpg",
+  title: "Formulário animado com JS puro e Animation | Jquery, Javascript, REACT, REACT NATIVE",
+  duration: "400 min",
+  price: "Free"
+  },
+  {
+  id: "jbAh5R8CH_o",
+  imgName:"imagem3.jpg",
+  title: "Criando aplicacoes WEB com REACT JS",
   duration: "57 min",
   price: "Free"
   },
   {
   id: "jbAh5R8CH_o",
-  imgName:"imagem1.jpg",
-  title: "Formulário animado com JS puro e Animation | kel kel",
-  duration: "57 min",
-  price: "Free"
-  },
-  {
-  id: "jbAh5R8CH_o",
-  imgName:"imagem1.jpg",
-  title: "Formulário animado com JS puro e Animation | kel kel",
-  duration: "57 min",
+  imgName:"imagem4.jpg",
+  title: "Curso Full Stack Javascript, node, frontend, mongoose, gulp, webpack, react",
+  duration: "800 min",
   price: "Free"
   },
   {
@@ -34,7 +34,7 @@ module.exports = [{
   price: "Free"
   },
   { id: "jbAh5R8CH_o",
-  imgName:"imagem1.jpg",
+  imgName:"imagem2.jpg",
   title: "Formulário animado com JS puro e Animation | kel kel",
   duration: "57 min",
   price: "Free"
